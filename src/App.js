ff24ca3054ea1375089065fa9112c8de787bae33
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-05-13 01:53:06 -0500

    npm installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
+const My404 = () => {
+  return (
+    <div>
+      You are lost
+    </div>
+    )
+}
 
 class App extends React.Component {
   constructor(){
+    console.log('constructor');
     super()
     this.state = {
-      
+
     }
 
   }
@@ -20,6 +27,13 @@ class App extends React.Component {
 
   return (
       <div className="container">
+        <nav className="navbar nvarbar-expand-lg navbar-light bg-light" >
+          <a>
+            <img src={logo} width="30" height="30"/>
+          </a>
+
+          
+        </nav>
         <h2>Late Night React Client API</h2>
       </div>
   
